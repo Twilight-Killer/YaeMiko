e40b971a02b2d3f44041058b593d851f6aa68e0c
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2023-12-21 05:59:49 +0700

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -85,8 +85,8 @@ GROUP_START_BTN = [
 
 ALIVE_BTN = [
     [
-        ib(text="UPDATES", url="https://t.me/Hydra_Updates"),
-        ib(text="SUPPORT", url="https://t.me/hydraXsupport"),
+        ib(text="UPDATES", url="https://t.me/HaoTogelLivedraw"),
+        ib(text="SUPPORT", url="https://t.me/HaoTogelLivedraw"),
     ],
     [
         ib(
@@ -97,7 +97,7 @@ ALIVE_BTN = [
 ]
 
 HELP_STRINGS = """
-🫧 *Yae-Miko* 🫧
+🫧 *Kimochi* 🫧
 
 ☉ *Here, you will find a list of all the available commands.*
 
